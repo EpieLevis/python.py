215bf6e15e5e46dce09adac8894c8d5805c05328
EpieLevis <113223361+EpieLevis@users.noreply.github.com>
2025-12-25 02:30:25 -0500

    Add files via upload

---
+++ name = 'Levis Epie'.py
@@ -0,0 +1,10 @@
+name = 'Levis Epie'
+street = "3513 sharonwood road"
+city = "Laurel"
+zip_code = "20724"
+phone = "3198219180"
+print("---contact information---")
+print(name)
+print(street)
+print(f'{city}, {street} {zip_code}')
+print(f"phone: {phone}")
---
+++ professor.py
@@ -0,0 +1,15 @@
+professor = "professor Albert Einstein"
+name = professor.removeprefix("professor")
+print(name)
+junior = "Ken Griffey Jr."
+name = junior.removesuffix("Jr.")
+print(name)
+doctor = "Doctor Zoidberg"
+name = doctor.removeprefix("doctor")
+print(name)
+mister = "Mr. Ed."
+name = mister.removeprefix("mr.")
+print = (name)
+senior = "Hank Williams sr."
+name = senior.removesuffix("sr.")
+print(name)
